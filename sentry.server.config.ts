ded00bf0484c30267eaedf7a2579366e8bb4d0ed
Aryan Agrawal <strongmozilla0216@gmail.com>
2026-05-06 13:02:14 +0530

    feat: integrate Sentry for error monitoring and performance tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a3ccc6ba3be3832a4b2671f5d1f96a83@o4511337719070720.ingest.us.sentry.io/4511337725231104",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
